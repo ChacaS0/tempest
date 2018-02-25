bb1f2a469652404fdf2a921ac2bd7d0cca649bbf
ChacaS0 <chk.chaca@gmail.com>
2018-02-25 15:12:01 +0400

    Fallback from aur version

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,10 +1,10 @@
 package cmd
 
-import "testing"
+// import "testing"
 
-func getPathsTest(t *testing.T) {
-	_, err := getPaths()
-	if err != nil {
-		t.Error("getPaths() could not read from the file .tempestcf and returned with this error:\n", err.Error())
-	}
-}
+// func getPathsTest(t *testing.T) {
+// 	_, err := getPaths()
+// 	if err != nil {
+// 		t.Error("getPaths() could not read from the file .tempestcf and returned with this error:\n", err.Error())
+// 	}
+// }
